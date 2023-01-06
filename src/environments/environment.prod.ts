231aec5b7dad66d40d7f3db150f398dfb1da52d3
Kumar Saptam <45693173+Sapython@users.noreply.github.com>
2023-01-06 11:12:42 +0530

    Added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'fbms-shreeva-demo',
+    appId: '1:403464137223:web:10688178b2a1cdd9a858a2',
+    storageBucket: 'fbms-shreeva-demo.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyATbn4lcWC9FwXTIHpA9iVyeADvdExh-bo',
+    authDomain: 'fbms-shreeva-demo.firebaseapp.com',
+    messagingSenderId: '403464137223',
+    measurementId: 'G-4Z2FYGH2CK',
+  },
   production: true
 };
