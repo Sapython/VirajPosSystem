a640eb6717e3273439d6b8b297f30639ae7671da
Saptam <saptampro2003@gmail.com>
2023-01-12 10:33:40 +0530

    Updated recrd system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { NgxIndexedDBService } from 'ngx-indexed-db';
-import { DatabaseService } from './services/database.service';
-const HSLToRGB = (h:any, s:any, l:any) => {
+import {Component, OnInit} from '@angular/core';
+import {NgxIndexedDBService} from 'ngx-indexed-db';
+import { Observable } from 'rxjs';
+import {DatabaseService} from './services/database.service';
+
+const HSLToRGB = (h: any, s: any, l: any) => {
   s /= 100;
   l /= 100;
   const k = (n: number) => (n + h / 30) % 12;
@@ -10,6 +12,7 @@ const HSLToRGB = (h:any, s:any, l:any) => {
     l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
   return [255 * f(0), 255 * f(8), 255 * f(4)];
 };
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,12 +20,16 @@ const HSLToRGB = (h:any, s:any, l:any) => {
 })
 export class AppComponent implements OnInit {
   title = 'VirajPosSystem';
-  constructor(public onlineDbService: DatabaseService,private dbService: NgxIndexedDBService) {}
+
+  constructor(public onlineDbService: DatabaseService, private dbService: NgxIndexedDBService) {
+  }
+
   counter = 0;
   added = 0;
   list: any[] = [];
-  people:any[] = [];
+  people: any[] = [];
   coloredDivs: any[] = [];
+  offlineData: Observable<any[]> | undefined;
   ngOnInit(): void {
     this.onlineDbService.getTasks().subscribe((doc) => {
       this.list = [];
@@ -31,34 +38,37 @@ export class AppComponent implements OnInit {
         this.list.push(doc);
       });
     });
+    this.getDataFromDb()
     for (let index = 0; index < 20; index++) {
       this.coloredDivs.push(this.getColor());
     }
     console.log(this.coloredDivs);
-    
+
   }
 
   getColor() {
     let mainOne = 360 * Math.random()
     let mainTwo = (25 + 70 * Math.random())
     let mainThree = (85 + 10 * Math.random())
-    return [`hsl(${mainOne}, ${mainTwo}%, ${mainThree}%)`,HSLToRGB(mainOne, mainTwo, mainThree)];
+    return [`hsl(${mainOne}, ${mainTwo}%, ${mainThree}%)`, HSLToRGB(mainOne, mainTwo, mainThree)];
   }
 
 
-  getDataFromDb(){
-    this.dbService.getAll('people').subscribe((people) => {
-      this.people = [];
-      console.log(people);
-    })
+  getDataFromDb() {
+    this.offlineData = this.dbService.getAll<any>('people')
+    // this.offlineData.subscribe((data) => {
+    //   console.log(data);
+      
+    // });
+    
   }
 
-  addToDb(){
+  addToDb() {
     return this.dbService.add('people', {
-    name: `Bruce Wayne`,
-    email: `bruce@wayne.com`,
-  }).subscribe(()=>{
-    this.getDataFromDb()
-  })
+      name: `Bruce Wayne`,
+      email: `bruce@wayne.com`,
+    }).subscribe(() => {
+      this.getDataFromDb()
+    })
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideFunctions,getFunctions } from '@angular/fire/functions';
-import { provideMessaging,getMessaging } from '@angular/fire/messaging';
-import { providePerformance,getPerformance } from '@angular/fire/performance';
-import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
-import { provideStorage,getStorage } from '@angular/fire/storage';
+import {
+  provideAnalytics,
+  getAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { providePerformance, getPerformance } from '@angular/fire/performance';
+import {
+  provideRemoteConfig,
+  getRemoteConfig,
+} from '@angular/fire/remote-config';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { DatabaseService } from './services/database.service';
 import { enableIndexedDbPersistence } from '@firebase/firestore';
@@ -21,23 +29,23 @@ import { EffectsModule } from '@ngrx/effects';
 
 import { DBConfig, NgxIndexedDBModule } from 'ngx-indexed-db';
 
-const dbConfig: DBConfig  = {
+const dbConfig: DBConfig = {
   name: 'MyDb',
   version: 1,
-  objectStoresMeta: [{
-    store: 'people',
-    storeConfig: { keyPath: 'id', autoIncrement: true },
-    storeSchema: [
-      { name: 'name', keypath: 'name', options: { unique: false } },
-      { name: 'email', keypath: 'email', options: { unique: false } }
-    ]
-  }]
+  objectStoresMeta: [
+    {
+      store: 'people',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        { name: 'name', keypath: 'name', options: { unique: false } },
+        { name: 'email', keypath: 'email', options: { unique: false } },
+      ],
+    },
+  ],
 };
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -46,8 +54,14 @@ const dbConfig: DBConfig  = {
     provideAuth(() => getAuth()),
     provideFirestore(() => {
       let fs = getFirestore();
-      enableIndexedDbPersistence(fs)
-      return fs
+      enableIndexedDbPersistence(fs).catch((err) => {
+        if (err.code == 'failed-precondition') {
+          alert('Multiple tabs open. Please close others to get offline support.')
+        } else if (err.code == 'unimplemented') {
+          alert('Offline Support unavailable on this browser')
+        }
+      });
+      return fs;
     }),
     provideFunctions(() => getFunctions()),
     provideMessaging(() => getMessaging()),
@@ -58,15 +72,13 @@ const dbConfig: DBConfig  = {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerWhenStable:30000',
     }),
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
-    NgxIndexedDBModule.forRoot(dbConfig)
+    NgxIndexedDBModule.forRoot(dbConfig),
   ],
-  providers: [
-    ScreenTrackingService,UserTrackingService,DatabaseService
-  ],
-  bootstrap: [AppComponent]
+  providers: [ScreenTrackingService, UserTrackingService, DatabaseService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
